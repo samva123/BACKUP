67ce56963547be54a5ae847809c988f8efdaf960
samva123 <navvarunb@gmail.com>
2024-09-01 00:13:33 +0530

    hello ji

---
+++ JAVASCRIPT/class1.js
@@ -0,0 +1,14 @@
+let msg = "       good Morning         ";
+
+let hello = msg.trim();
+
+console.log(msg.toUpperCase());
+
+console.log(msg.toLowerCase());
+
+let mesg = "how r u";
+console.log(mesg.indexOf('r'));
+console.log(hello.indexOf('o'));
+
+
+console.log(hello.repeat(mesg));
